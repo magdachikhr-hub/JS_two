831408a3d4f8606389f3ba384fc53d259777193b
Magda Chikhradze <magda292005@gmail.com>
2026-03-17 16:53:30 +0400

    learn

---
+++ main.js
@@ -0,0 +1,26 @@
+// let color = "red";
+
+// let number = 67;
+
+// let isSummer = false;
+// let isSpring = true;
+
+// let arr = ["blue", 1, 64, "hallowr"];
+
+// let user = {};
+
+// let armor = {
+//   color: "black",
+//   health: 10,
+//   size: 2,
+// };
+
+// console.log(color);
+
+// let age = 20;
+let age;
+
+let userAge = 22;
+userAge = null;
+
+console.log(userAge);
