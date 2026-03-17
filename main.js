df9e836048f0e051d9cc026bfabb7c321915c187
Magda Chikhradze <magda292005@gmail.com>
2026-03-17 17:43:26 +0400

    text

--- main.js
+++ main.js
@@ -18,9 +18,24 @@
 // console.log(color);
 
 // let age = 20;
-let age;
+// let age;
 
-let userAge = 22;
-userAge = null;
+// let userAge = 22;
+// userAge = null;
 
-console.log(userAge);
+// console.log(userAge);
+
+let text = "javascript";
+
+console.log(text.charAt(2));
+
+console.log(text.length);
+console.log(text.indexOf("p"));
+console.log(text.lastIndexOf("a"));
+console.log(text.toLocaleLowerCase());
+console.log(text.toLocaleUpperCase());
+
+let age = 23;
+let name = "user";
+
+console.log("hi , my name is " + name  + " my age is: " + age );
